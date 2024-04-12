a8d4b8497bb392e90d4d5a91928ebc0528dc28d0
Maksim Tkashev <maks93makula@gmail.com>
2024-04-12 09:49:19 +0300

    Создал схему Mongoose в файле Link.ts, сделана типизация types.d.ts, указал в этом коммите

---
+++ api/lunkModelsOn/Link.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const LinksSchema = new Schema({
+  shortUrl: {
+    type: String,
+    required: true,
+  },
+  originalUrl: {
+    type: String,
+    required: true,
+  },
+});
+
+const Link = mongoose.model('Link', LinksSchema);
+
+export default Link;
