be92b2eac273d6bcddc9a90b3c97ea8f41bc9647
Maksim Tkashev <maks93makula@gmail.com>
2024-04-12 09:43:52 +0300

    Настройка бэкенда и базы данных, добавил express, сделал роутинг к будущему файлу, сделана настройка к MondoDB в конфиге. обработка ошибок

---
+++ api/index.ts
@@ -0,0 +1,36 @@
+import cors from 'cors';
+import express from 'express';
+import mongoose from "mongoose";
+import config from "./config";
+import linksRouter from "./routers/links";
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/links', linksRouter);
+
+async function launchApp() {
+    try {
+        await mongoose.connect(config.mongoose.db);
+    } catch (error) {
+        console.error('Ошибка не удалось подключиться к MongoDB', error);
+        process.exit(1);
+    }
+
+    app.listen(port, () => {
+        console.log(`Сервер стартовал на порту: ${port}`)
+    });
+
+    process.on('SIGTERM', async () => {
+        console.log("Server is shutting down...");
+        await mongoose.disconnect();
+        console.log("MongoDB disconnected.");
+        process.exit(0);
+    });
+}
+
+
+void launchApp();
